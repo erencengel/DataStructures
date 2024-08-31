a1bd973f191be73f925367964ef3656731e52b16
erencengel <erencengel@gmail.com>
2024-08-31 18:04:09 +0300

    second solution

---
+++ src/test/java/day1/Assignment2_NumOfPeopleInTheBus.java
@@ -0,0 +1,25 @@
+package day1;
+
+import java.util.ArrayList;
+
+public class Assignment2_NumOfPeopleInTheBus {
+
+    public static void main(String[] args) {
+
+        ArrayList<int[]> busStops = new ArrayList<>();
+        busStops.add(new int[] {10,0});
+        busStops.add(new int[] {3,5});
+        busStops.add(new int[] {2,5});
+        System.out.println("countPassengers(busStops) = " + countPassengers(busStops));
+
+    }
+
+    public static int countPassengers(ArrayList<int[]> stops) {
+        int numOfPeople = 0;
+        for(int i = 0; i < stops.size(); i++) {
+            numOfPeople = numOfPeople + stops.get(i)[0] - stops.get(i)[1];
+        }
+        return numOfPeople;
+    }
+
+}
