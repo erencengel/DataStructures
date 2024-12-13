b730bc4f955f67012fa04942a9dcd449df5149ff
erencengel <erencengel@gmail.com>
2024-12-13 12:01:06 +0300

    tree is started

---
+++ src/test/java/Trees_6/MyTree.java
@@ -0,0 +1,8 @@
+package Trees_6;
+
+public class MyTree {
+
+    TNode root;
+
+    public MyTree() {}
+}
---
+++ src/test/java/Trees_6/TNode.java
@@ -0,0 +1,12 @@
+package Trees_6;
+
+public class TNode {
+
+    int value;
+    TNode leftChild;
+    TNode rightChild;
+
+    public TNode(int value) {
+        this.value = value;
+    }
+}
